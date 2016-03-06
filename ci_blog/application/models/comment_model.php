a25ffb8e47d0c42e8ca3532e9ceb31261490383f
Your Name <email@example.com>
2016-03-06 16:18:10 +0800

    dele

--- ci_blog/application/models/comment_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    class Comment_model extends CI_Model{
-    /*添加评论*/
-    public function add($data){
-            $this->db->insert('message',$data);
-        }
-    }
- ?>
